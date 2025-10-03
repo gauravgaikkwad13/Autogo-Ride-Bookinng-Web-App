4004e0807a295e6c1136f9e8e6a0066306e2c4c6
gauravgaikkwad13 <gauravgaikkwad@gmail.com>
2025-10-03 16:19:41 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,7 +15,7 @@ createRoot(document.getElementById('root')).render(
       <UserContext>
         <SocketProvider>
           <BrowserRouter>
-            <App />
+            <Appa />
           </BrowserRouter>
         </SocketProvider>
       </UserContext>
